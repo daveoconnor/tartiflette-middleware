f7ae1833156ec920703312dd4c10f9815e0de263
Dave O'Connor <doc+git@skynet.ie>
2021-04-17 11:18:25 -0700

    add pypi build script

---
+++ pypi-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python3 -m virtualenv --python=python3.8 venv
+source venv/bin/activate
+python3 --version
+python3 setup.py test
+if [[ $? == 1 ]]; then
+	exit 1
+fi;
+python3 -m pip install twine
+python3 setup.py sdist bdist_wheel
+python3 -m twine upload -u __token__ -p $TEST_TOKEN --non-interactive --repository testpypi dist/*
+python3 -m twine upload -u __token__ -p $LIVE_TOKEN --non-interactive dist/*
